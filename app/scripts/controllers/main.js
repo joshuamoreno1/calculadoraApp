32e9241907eee7389ca274f31dd8b56d6cde853a
joshuamoreno1 <joshuamoreno1@gmail.com>
2015-10-03 00:03:46 -0500

    Creacion de la estructura base del proyecto con la configuracion para la ejecucion de pruebas unitarias y E2E.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name calculadoraApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the calculadoraApp
+ */
+angular.module('calculadoraApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.mensajeInicial = 'Calculadora App';
+  });
---
+++ app/scripts/services/mathservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name calculadoraAppApp.Mathservice
+ * @description
+ * # Mathservice
+ * Service in the calculadoraAppApp.
+ */
+angular.module('calculadoraApp')
+  .service('Mathservice', function Mathservice() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
